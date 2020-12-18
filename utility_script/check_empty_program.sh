7c914ddfe4dbb006a06fe43d3af8378586cf3a1b
Jie Ni <nijie3212outlook.com>
2020-12-18 10:34:14 -0600

    update check empty program

--- utility_script/check_empty_program.sh
+++ utility_script/check_empty_program.sh
@@ -2,14 +2,17 @@
 
 program_file_dir="/home/pi/openbehavior/SocialDrinking/python"
 
-num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($7==0) print($9)}' | wc -l`
+num_empty_files=`ls -l $program_file_dir/*.py | awk '{if($5==0) print($9)}' | wc -l`
 
 
 if [ $num_empty_files -gt 0 ]
 then
 	cd /home/pi
+	
+	rm -rf ./openbehavior
+	mkdir openbehavior && cd openbehavior
 
-	redownload_code=`rm -rf ./openbehavior && git clone https://github.com/chen42/openbehavior.git`
+	redownload_code=`git clone https://github.com/nijie321/SocialDrinking.git`
 	while kill -0 $redownload_code ; do
 		echo "Program is being downloded. Please Wait....."
 		sleep 1
