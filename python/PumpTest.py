38d288183114b81d44d98ddb53cfac6455cb3e5d
Jie Ni <nijie321@outlook.com>
2021-03-18 06:28:43 -0500

    modify pump test. 5 touches reward activation

--- python/PumpTest.py
+++ python/PumpTest.py
@@ -1,89 +1,46 @@
 
-from time import sleep
-import RPi.GPIO as GPIO
 from gpiozero import Button
 from gpiozero import DigitalInputDevice
+from pump_move import PumpMove
 
-GPIO.setwarnings(False)
+import board
+import busio
+import adafruit_mpr121
 
-request = input("""
-                Enter a number to start a test:
-                    1 - Test left and right buttons
-                    2 - Test pump forward limit
-                    3 - Test Pump backward limit
-                    4 - Test pump move
-                """)
 
-while True:
+def pump_test():
 
-    if request == 1:
-        left_button = Button("GPIO5")
-        right_button = Button("GPIO27")
+    # placeholder
+    command = "123"
 
-        def left_btn_press():
-            while left_button.value == 1:
-                print("left button pressed")
+    # command = input("Please scan the ")
+    act_count = 0
+    while command == "123":
+        i2c = busio.I2C(board.SCL, board.SDA)
+        mpr121 = adafruit_mpr121.MPR121(i2c)
 
-        def right_btn_press():
-            while right_button.value == 1:
-                print("right button pressed")
+        mover = PumpMove()
+        forwardbtn = Button("GPIO5")
+        backwardbtn = Button("GPIO27")
 
-        left_button.when_pressed = left_btn_press
-        right_button.when_pressed = right_btn_press
+        BACKWARD_LIMIT_BTN = "GPIO23"
+        BACKWARD_LIMIT = DigitalInputDevice(BACKWARD_LIMIT_BTN)
 
-    elif request == 2:
-        forward_limit = GPIO.setup("24", GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        def forward():
+            while forwardbtn.value == 1:
+                mover.move("forward")
+
+        def backward():
+            while BACKWARD_LIMIT.value != 1:
+                mover.move("backward")
+
+        forwardbtn.when_pressed = forward
+        backwardbtn.when_pressed = backward
+
+        act = mpr121.touched_pins[1]
         
-        if GPIO.input(24):
-            print("forward limit pressed.")
-
-    elif request == 3:
-        backward_limit = DigitalInputDevice("GPIO23")
-
-        if backward_limit.value == 1:
-            print("backward limit pressed")
-            
-    elif request == 4:
-        steps = 200
-        rotate_dir = 1
-        DIR = 26
-        STEP = 6
-        CW = 1
-        CCW = 0
-        GPIO = GPIO
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(DIR, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.setup(STEP, GPIO.OUT, initial=GPIO.HIGH)
-        GPIO.output(DIR,CW)
-        MODE = (17,22)
-        GPIO.setup(MODE, GPIO.OUT)
-        RESOLUTION = {
-                            'Full': (0,0),
-                            'Half': (1,0),
-                            '1/8': (0,1),
-                            '1/16': (1,1),
-                          }
-        
-        step_counts = steps
-        delay = .0209 / 50
-
-        GPIO.output(STEP, GPIO.HIGH)
-        GPIO.output(DIR, CW)
-        for step in range(steps):
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay)
-
-        
-        GPIO.output(DIR, CCW)
-        for step in range(steps):
-            GPIO.output(STEP, GPIO.HIGH)
-            sleep(delay)
-            GPIO.output(STEP, GPIO.LOW)
-            sleep(delay)
-        
-        
-        GPIO.cleanup(MODE)
-        
-        
+        if act:
+            if act_count % 5 == 0:
+                mover.move("forward")
+                act_count = 0
+            act_count += 1
