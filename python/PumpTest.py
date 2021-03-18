ec1a61aa1106829f92f266a57605e99776f4f97c
Jie Ni <nijie3212outlook.com>
2021-03-18 10:02:25 -0500

    another mistake

--- python/PumpTest.py
+++ python/PumpTest.py
@@ -11,32 +11,32 @@ import adafruit_mpr121
 def pump_test():
     # command = input("Please scan the ")
     act_count = 0
-    while command == "123":
-        i2c = busio.I2C(board.SCL, board.SDA)
-        mpr121 = adafruit_mpr121.MPR121(i2c)
 
-        mover = PumpMove()
-        forwardbtn = Button("GPIO5")
-        backwardbtn = Button("GPIO27")
+    i2c = busio.I2C(board.SCL, board.SDA)
+    mpr121 = adafruit_mpr121.MPR121(i2c)
 
-        BACKWARD_LIMIT_BTN = "GPIO23"
-        BACKWARD_LIMIT = DigitalInputDevice(BACKWARD_LIMIT_BTN)
+    mover = PumpMove()
+    forwardbtn = Button("GPIO5")
+    backwardbtn = Button("GPIO27")
 
-        def forward():
-            while forwardbtn.value == 1:
-                mover.move("forward")
+    BACKWARD_LIMIT_BTN = "GPIO23"
+    BACKWARD_LIMIT = DigitalInputDevice(BACKWARD_LIMIT_BTN)
 
-        def backward():
-            while BACKWARD_LIMIT.value != 1:
-                mover.move("backward")
+    def forward():
+        while forwardbtn.value == 1:
+            mover.move("forward")
 
-        forwardbtn.when_pressed = forward
-        backwardbtn.when_pressed = backward
+    def backward():
+        while BACKWARD_LIMIT.value != 1:
+            mover.move("backward")
 
-        act = mpr121.touched_pins[1]
-        
-        if act:
-            if act_count % 5 == 0:
-                mover.move("forward")
-                act_count = 0
-            act_count += 1
+    forwardbtn.when_pressed = forward
+    backwardbtn.when_pressed = backward
+
+    act = mpr121.touched_pins[1]
+    
+    if act:
+        if act_count % 5 == 0:
+            mover.move("forward")
+            act_count = 0
+        act_count += 1
