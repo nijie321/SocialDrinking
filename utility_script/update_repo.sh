8cbb08dbaf3a162d655a1aad48c5c5bb8bfdc5bb
Jie Ni <nijie3212outlook.com>
2021-05-10 10:42:22 -0500

    clone instead of pull

--- utility_script/update_repo.sh
+++ utility_script/update_repo.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-cd /home/pi/openbehavior/PeerPub/
 
-git reset --hard && git pull --depth 1
+cd /home/pi/openbehavior/ && rm -rf PeerPub/ && cd /home/pi/openbehavior/ && git clone https://www.github.com/nijie321/PeerPub.git
 
 echo $(date +"%m-%d-%y %T") $(git log -1 --pretty=oneline) >> "/home/pi/SocialDrinking/${BOXID}_update"
 
