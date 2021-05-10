e286ebdd8255c66fe09affebc590e02df99b0102
Jie Ni <nijie3212outlook.com>
2021-05-10 10:39:50 -0500

    update

--- utility_script/update_repo.sh
+++ utility_script/update_repo.sh
@@ -7,4 +7,4 @@ git reset --hard && git pull --depth 1
 echo $(date +"%m-%d-%y %T") $(git log -1 --pretty=oneline) >> "/home/pi/SocialDrinking/${BOXID}_update"
 
 
-bash /home/pi/openbehavior/PeerPub/wifi_network/rsync.sh
+bash /home/pi/openbehavior/PeerPub/wifi-network/rsync.sh
