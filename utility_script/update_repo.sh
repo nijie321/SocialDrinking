ef8f72f9eb6ae05d6a555bbff72e4aea39fad607
Jie Ni <nijie3212outlook.com>
2021-05-10 10:32:20 -0500

    sync to dropbox after github update

--- utility_script/update_repo.sh
+++ utility_script/update_repo.sh
@@ -6,6 +6,5 @@ git reset --hard && git pull --depth 1
 
 echo $(date +"%m-%d-%y %T") $(git log -1 --pretty=oneline) >> "/home/pi/SocialDrinking/${BOXID}_update"
 
-sleep 30
 
-sudo reboot
+bash /home/pi/openbehavior/PeerPub/wifi_network/rsync.sh
