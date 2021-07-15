4b7bf84364dd054038645a0da8efe9caa3b06cc7
miraclezero <flmajia@gmail.com>
2021-07-15 16:28:19 -0500

    Sync with RFID key

--- wifi-network/rsync.sh
+++ wifi-network/rsync.sh
@@ -4,26 +4,37 @@
 delay=`shuf -i1-3 -n1` 
 
 # make sure wlan0 is up
-ifconfig wlan0 up
-while [ `sudo ifconfig wlan0 |grep broadcast |wc -l` -ne 1 ]
-	do
-	sleep 2
-	cnt=$[$cnt+1]
-	if [ $cnt -eq 30 ] # max 60 sec  
-		then 
-			break
-	fi
-done
+#ifconfig wlan0 up
+#while [ `sudo ifconfig wlan0 |grep broadcast |wc -l` -ne 1 ]
+#	do
+#	sleep 2
+#	cnt=$[$cnt+1]
+#	if [ $cnt -eq 30 ] # max 60 sec  
+#		then 
+#			break
+#	fi
+#done
 
-# remove files older than 15 days
+# remove files older than 15 days but keep git update files
 rm `find /home/pi/SocialDrinking/* -mtime +15 |grep -v update` 
 
 gzip -f /home/pi/SocialDrinking/*csv
 
 rsync -auvp -e ssh /home/pi/SocialDrinking/ root@149.56.128.122:~/Dropbox/Pies/SocialDrinking/ 
-sleep 1m
+sleep 15
 #sync one more time
-rsync -auvp -e ssh /home/pi/SocialDrinking/ root@149.56.128.122:~/Dropbox/Pies/SocialDrinking/ 
+#rsync -auvp -e ssh /home/pi/SocialDrinking/ root@149.56.128.122:~/Dropbox/Pies/SocialDrinking/ 
 
-sudo reboot
+echo "Use sync key to sync data again, or scan any other key to reboot"
+read var
+if [[ $var = "0086eafa" || $var = "002d5494" ]]; then
+        echo "Sync data again"
+rsync -auvp -e ssh /home/pi/SocialDrinking/ root@149.56.128.122:~/Dropbox/Pies/SocialDrinking/
+echo "If sync fails, scan sync key to sync again after reboot"
+sleep 15
+sudo reboot -f
+else
+        echo "reboot system"
+sudo reboot -f
 
+fi
